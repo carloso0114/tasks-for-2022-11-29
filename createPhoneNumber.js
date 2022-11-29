6c608fde940a2a82f9fecc34f448b46fa384f692
carloso0114 <carlossospinag@gmail.com>
2022-11-29 00:21:49 -0500

    upload solution task 2

---
+++ createPhoneNumber.js
@@ -0,0 +1,14 @@
+function createPhoneNumber(numbers){
+  let result = ['(','','','',')',' ','','','','-','','','','']
+  result[1] = numbers[0]
+  result[2] = numbers[1]
+  result[3] = numbers[2]
+  result[6] = numbers[3]
+  result[7] = numbers[4]
+  result[8] = numbers[5]
+  result[10] = numbers[6]
+  result[11] = numbers[7]
+  result[12] = numbers[8]
+  result[13] = numbers[9]
+  return result.join('')
+}
