002e4713ea6e004c923a13dde5eb9bf52c0a9a57
carloso0114 <carlossospinag@gmail.com>
2022-11-29 00:22:32 -0500

    upload solution task 3

---
+++ generateHashtag.js
@@ -0,0 +1,15 @@
+function generateHashtag (str) {
+  function capitalizeWords(arr) {
+    return arr.map(element => {
+      return element.charAt(0).toUpperCase() + element.slice(1);
+    });
+  }
+  length = str.split(" ").join("").length
+  if (str === '' || length === 0 || length >= 140) {
+    return false
+  } else {
+    str = str.split(' ')
+    str = capitalizeWords(str)
+    return '#'+str.join('')
+  }
+}
