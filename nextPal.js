8b733787ae15f5185c24eca3a9b45e25dc74db25
carloso0114 <carlossospinag@gmail.com>
2022-12-01 12:55:03 -0500

    SFT-2 Add semicolons

--- nextPal.js
+++ nextPal.js
@@ -1,21 +1,21 @@
 function nextPal(val) {
   function isPalindrome(str) {
-    str = String(str)
+    str = String(str);
     let len = str.length;
     let mid = Math.floor(len / 2);
     for (var i = 0; i < mid; i++) {
       if (str[i] !== str[len - 1 - i]) {
         return false;
-      }
-    }
+      };
+    };
     return true;
   }
   while (true) {
     val += 1;
     if (isPalindrome(val) === true) {
-      return val
-    }
-  }
-}
+      return val;
+    };
+  };
+};
 
-console.log(nextPal(11))
+console.log(nextPal(11));
