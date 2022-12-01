bf0b089cb55c246892ce15b0838a7e1b67caa7e5
carloso0114 <carlossospinag@gmail.com>
2022-12-01 12:52:22 -0500

    SFT-1 Add semicolons on every line

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -1,16 +1,16 @@
 function createPhoneNumber(numbers){
-  let result = ['(','','','',')',' ','','','','-','','','','']
-  result[1] = numbers[0]
-  result[2] = numbers[1]
-  result[3] = numbers[2]
-  result[6] = numbers[3]
-  result[7] = numbers[4]
-  result[8] = numbers[5]
-  result[10] = numbers[6]
-  result[11] = numbers[7]
-  result[12] = numbers[8]
-  result[13] = numbers[9]
-  return result.join('')
+  let result = ['(','','','',')',' ','','','','-','','','',''];
+  result[1] = numbers[0];
+  result[2] = numbers[1];
+  result[3] = numbers[2];
+  result[6] = numbers[3];
+  result[7] = numbers[4];
+  result[8] = numbers[5];
+  result[10] = numbers[6];
+  result[11] = numbers[7];
+  result[12] = numbers[8];
+  result[13] = numbers[9];
+  return result.join('');
 }
 
-console.log(createPhoneNumber([1,2,3,4,5,6,7,8,9,0]))
+console.log(createPhoneNumber([1,2,3,4,5,6,7,8,9,0]));
