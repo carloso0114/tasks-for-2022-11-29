3c0ad74fdb2e884ea8ed83fa3430451d706b16e7
carloso0114 <carlossospinag@gmail.com>
2022-12-01 12:57:10 -0500

    SFT-3 add missing semicolons

--- generateHashtag.js
+++ generateHashtag.js
@@ -3,15 +3,15 @@ function generateHashtag (str) {
     return arr.map(element => {
       return element.charAt(0).toUpperCase() + element.slice(1);
     });
-  }
+  };
   length = str.split(" ").join("").length
   if (str === '' || length === 0 || length >= 140) {
-    return false
+    return false;
   } else {
-    str = str.split(' ')
-    str = capitalizeWords(str)
-    return '#'+str.join('')
-  }
-}
+    str = str.split(' ');
+    str = capitalizeWords(str);
+    return '#'+str.join('');
+  };
+};
 
-console.log(generateHashtag('Soft serve rules'))
+console.log(generateHashtag('Soft serve rules'));
